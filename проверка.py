12f4f52b96777dab491570c0932924413eabd8ee
enotalam <blakshina.lera@gmail.com>
2024-02-25 22:58:26 +0300

    чек 2

--- проверка.py
+++ проверка.py
@@ -1,26 +1,30 @@
-
+from random import shuffle, sample
 from csv import reader, writer
 
-def hash(user):
-    user= user.replace(' ','')
-    hash_cod = 0
-    letters = '_абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-    p = 67
-    m = 10**9 + 9
-    for i in range(len(user)):
-        s = letters.find(user[i])
-        hash_cod = (hash_cod +s*(p**i)%m)%m
-    return hash_cod
+def login(pupil):
+    pupil = pupil.split()
+    login = pupil[0]+'_'+pupil[1][0]+pupil[2][0]
+    return login
+def password():
+    letters = 'qwertyuiopasdfghjklzxcvbnm'
+    number = '1234567890'
+    password_pupil = sample(letters.upper(),1)
+    password_pupil += sample(letters, 1)
+    password_pupil += sample(number, 6)
+    shuffle(password_pupil)
+    return ''.join(password_pupil)
 
 with open('students.csv',encoding='utf-8') as data_file:
     students = list(reader(data_file,delimiter=','))
     head_line = students.pop(0)
 
-for i in range (len(students)):
-    user_name = str(students[i][1])
-    students[i][0] = hash(user_name)
+for pupil in students:
+    pupil.append(login(pupil[1]))
+    pupil.append(password())
 
-with open('students_password2.csv', 'w', encoding='utf-8') as data_file:
-    data_writer = writer(data_file, delimiter=',')
-    data_writer.writerow(head_line)
-    data_writer.writerows(students)
+with open('students_login.csv','w', encoding='utf-8') as data_file:
+    students_writer = writer(data_file,delimiter=',')
+    head_line.append('login')
+    head_line.append('password')
+    students_writer.writerow(head_line)
+    students_writer.writerows(students)
