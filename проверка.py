1aaaafa8369139e42c64d6a8b95022d16de637d0
enotalam <blakshina.lera@gmail.com>
2024-02-26 00:50:48 +0300

    чек 3

--- проверка.py
+++ проверка.py
@@ -2,20 +2,27 @@
 from csv import reader, writer
 
 with open('students.csv',encoding='utf-8') as data_file:
-    students = list(reader(data_file,delimiter=','))
-    head_lines = students.pop(0)
+    students_nom = list(reader(data_file,delimiter=','))
+    head_lines = students_nom.pop(0)
+students = list()
 
-id_pupil = input('Введите айди или стоп ')
+for i in students_nom:
+    if '10' in i[3] and i[-1]!= 'None':
+        students.append(i)
 
-while id_pupil != 'STOP':
-    position = 1
-    for pupil in students:
-        if pupil[2] == id_pupil:
-            pupil_name = pupil[1].split()
-            grade = pupil[-1]
-            position = 0
-    if position ==0:
-        print(f'Проект номер {id_pupil} делал {pupil_name[1][0]}. {pupil_name[0]} он получил{grade}')
-    if position==1 :
-        print('no')
-    id_pupil = input('Введите айди или стоп ')
+for i in range(1, len(students)):
+    if int(students[i][-1]) > int(students[i-1][-1]):
+        pos = i
+        while pos != 0 and int(students[pos][-1]) > int(students[pos-1][-1]):
+            item = students[pos]
+            students[pos] = students[pos - 1]
+            students[pos - 1] = item
+            pos-=1
+
+print('10 класс')
+for i in range(3):
+    full_name = students[i][1]
+    full_name = full_name.split()
+    name = full_name[1]
+    second_name = full_name[0]
+    print(f"{str(i + 1)} место: {name[0]}. {second_name}")
