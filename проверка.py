6eb74fe588f85b3fede7cbeb76e024a7aa35397e
enotalam <blakshina.lera@gmail.com>
2024-02-25 23:56:33 +0300

    чек 3

--- проверка.py
+++ проверка.py
@@ -1,30 +1,21 @@
-from random import shuffle, sample
-from csv import reader, writer
 
-def login(pupil):
-    pupil = pupil.split()
-    login = pupil[0]+'_'+pupil[1][0]+pupil[2][0]
-    return login
-def password():
-    letters = 'qwertyuiopasdfghjklzxcvbnm'
-    number = '1234567890'
-    password_pupil = sample(letters.upper(),1)
-    password_pupil += sample(letters, 1)
-    password_pupil += sample(number, 6)
-    shuffle(password_pupil)
-    return ''.join(password_pupil)
+from csv import reader, writer
 
 with open('students.csv',encoding='utf-8') as data_file:
     students = list(reader(data_file,delimiter=','))
-    head_line = students.pop(0)
+    head_lines = students.pop(0)
 
-for pupil in students:
-    pupil.append(login(pupil[1]))
-    pupil.append(password())
+id_pupil = input('Введите айди или стоп ')
 
-with open('students_login.csv','w', encoding='utf-8') as data_file:
-    students_writer = writer(data_file,delimiter=',')
-    head_line.append('login')
-    head_line.append('password')
-    students_writer.writerow(head_line)
-    students_writer.writerows(students)
+while id_pupil != 'STOP':
+    position = 1
+    for pupil in students:
+        if pupil[2] == id_pupil:
+            pupil_name = pupil[1].split()
+            grade = pupil[-1]
+            position = 0
+    if position ==0:
+        print(f'Проект номер {id_pupil} делал {pupil_name[1][0]}. {pupil_name[0]} он получил{grade}')
+    if position==1 :
+        print('no')
+    id_pupil = input('Введите айди или стоп ')
